b193be6236ddae8f776dcf839a879a0845bdf0f5
Paulo Monteiro <pauloesquilo@gmail.com>
2019-05-05 08:35:14 -0700

    cart service instrumented by New Relic

--- TRAINING_GUIDE.sh
+++ TRAINING_GUIDE.sh
@@ -78,7 +78,7 @@ git config --global user.email "${GITHUB_USER_EMAIL}"
 
 cd ~ && git clone "http://github.com/${GITHUB_USER}/${GITHUB_REPO}"
 cd ~/${GITHUB_REPO}
-git checkout --track origin/S00-no-instrumentation
+git checkout --track origin/S01-cart-service-instrumented
 
 #
 # ATTENTION: WE NEED TO CHANGE THE ACTIVE BRANCH HERE
